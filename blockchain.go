ea46448688ecde8f234cc28f12c0a62845301c79
stinkymonkeyph <monkeydev.team@gmail.com>
2024-05-29 03:44:28 +0800

    feat: add initial block structure

--- blockchain.go
+++ blockchain.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string, transactions []string) *Block {
+	b := new(Block)
+
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	b.transactions = transactions
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp\t %d \n", b.timestamp)
+	fmt.Printf("nonce\t %d \n", b.nonce)
+	fmt.Printf("previousHash\t %s \n", b.previousHash)
+	fmt.Printf("transactions\t %s \n", b.transactions)
+}
+
+func init() {
+	log.SetPrefix("Blockchain Node: ")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	b := NewBlock(0, "root hash", []string{"root tx"})
+	b.Print()
 }
